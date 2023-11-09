77c1791d5ceb66b335c83a4bd292da8118d856d3
aftababu <aftababu54@email.com>
2023-11-09 13:31:33 +0600

    second commit 5

--- backend/server.js
+++ backend/server.js
@@ -44,21 +44,16 @@ app.use("/api/v1", chatsRoutes);
 app.use("/api/v1", messageRoutes);
 app.use(errorMiddleware);
 const __dirname1 = path.resolve();
-<<<<<<< HEAD
+
 
   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
   });
-=======
 
-app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
-});
->>>>>>> a9f3155 (second commit 3)
+
 
 const server = app.listen(process.env.PORT, () => {
   console.log("🇵🇸🇵🇸🇵🇸🇵🇸🇵🇸🇵🇸🇵🇸🇵🇸");
